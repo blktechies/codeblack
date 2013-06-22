617916c6d05d63a94a683171f05c07648d70a91c
Kyle Wanamaker <kyle@tumblr.com>
2013-06-22 15:03:14 -0400

    Add Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Codeblack::Application.config.secret_token = 'fa768acef189b4e36540e591ce6a1b06d8e21d1a91a9e17723bbb0dfd863e3675f7941b92e757bb3ef66a1b9fe83e33db45188811eda3be9fd76681f80285c4f'
